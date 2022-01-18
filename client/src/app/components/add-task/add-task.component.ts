5e16c19f94dd38f16948fcc7285e5136d1fa4a0b
Fetah .OK <ouldkradda.fateh@gmail.com>
2022-01-18 10:32:05 +0000

    🎉 addTask component

---
+++ client/src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
+import { Task } from 'src/app/Task';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css'],
+})
+export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
+  text: string = '';
+  day: string = '';
+  reminder: boolean = false;
+  isAddTaskHidden: boolean = true;
+  subscription: Subscription | undefined;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.isAddTaskHidden = value));
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add a task to save !');
+      return;
+    }
+
+    const newTask: Task = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder,
+    };
+
+    // emit event
+    this.onAddTask.emit(newTask);
+
+    // clear form
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
+}
